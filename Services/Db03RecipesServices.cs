030b45cd7ba71419cf7113e7607687efdaa42f97
Frédéric COLIN <frederic.colin.31@gmail.com>
2025-11-19 11:40:23 +0100

    Db03

---
+++ Services/Db03RecipesServices.cs
@@ -0,0 +1,45 @@
+﻿using DataContracts;
+using Microsoft.Data.SqlClient;
+using Services.Core;
+using ServicesContracts;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class Db03RecipesServices : AbstractRecipesServices
+    {
+        public override List<Recipe> GetAll()
+        {
+            var recipes = new List<Recipe>();
+
+            var factory = SqlClientFactory.Instance;
+
+            using (var cn = factory.CreateConnection())
+            {
+                cn.ConnectionString = "ConnectionStrings:RecipesConnectionString".GetValueFor();
+
+                cn.Open();
+
+                var cmd = cn.CreateCommand();
+
+                cmd.CommandText = "sSelectRecipes";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    recipes.Add(new Recipe() { Id = Guid.Parse(reader["id"].ToString()), Title = reader["title"].ToString() });
+                }
+            }
+
+            return recipes;
+        }
+
+    }
+}
+
